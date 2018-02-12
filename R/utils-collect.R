ec0e2a2e442a5aef2f6a8ee90632a07edbd3ec02
Evan Odell <evanodell91@gmail.com>
2018-02-12 12:02:00 +0000

    possible way around curl problems?

--- R/utils-collect.R
+++ R/utils-collect.R
@@ -3,14 +3,13 @@
 ## utility function to download data
 
 
-nomis_collect_util <- function(query){
-  
-  df <- suppressMessages(readr::read_csv(
-    paste0("https://www.nomisweb.co.uk/api/v01/dataset", query)
-    ))
-  
-  df
-  
+nomis_collect_util <- function(query) {
+    
+    df <- suppressMessages(readr::read_csv(paste0("https://www.nomisweb.co.uk/api/v01/dataset", 
+        query)))
+    
+    df
+    
 }
 
 

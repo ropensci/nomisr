7c879328cc3ad099f09208235ace85df04a84dcd
Evan Odell <evanodell91@gmail.com>
2018-03-22 12:28:16 -0400

    update inclusion of loadpkg

--- R/nomisr-package.R
+++ R/nomisr-package.R
@@ -17,7 +17,7 @@
 #'
 #' @docType package
 #' @name nomisr
-#' @importFrom jsonlite fromJSON
+#' @importFrom jsonlite fromJSON loadpkg
 #' @importFrom tibble as_tibble enframe
 #' @importFrom readr read_csv
 #' @importFrom dplyr bind_rows

d2515bf8207e6a7d8a94d57d357d025fba7e7878
evan <evanodell91@gmail.com>
2020-02-03 08:44:41 +0000

    better error messages

--- R/utils-get-data.R
+++ R/utils-get-data.R
@@ -29,16 +29,23 @@ nomis_get_data_util <- function(query) {
     )
   },
   error = function(cond) {
+    err <- conditionMessage(cond)
+    if (startsWith(err, "Cannot read file")) {
+      message("It is likely you have included a parameter that is ",
+              "not available to query in this dataset.\n\n",
+              "Here's the original error message:\n", cond)
+    } else {
     message(
       "It is likely that you have been automatically rate limited ",
       "by the Nomis API.\n",
       "You can make smaller data requests, or try again later.\n\n",
       "Here's the original error message:\n", cond
     )
+    }
 
     return(NA)
   }, warning = function(cond) {
-    stop("The API request did not return any results.\n",
+    stop("The API request did not return any results. ",
       "Please check your parameters.",
       call. = FALSE
     )
